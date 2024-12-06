9f746ab9fe024dcabaf31512d751f24a17b63e69
JoaoPedro-Reis <joaoreis.pedro@estudante.ufjf.br>
2024-12-05 22:19:11 -0300

    adiociona alguns estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import Animal from './Animal'
-import Exibicao from '/Exibicao'
+import Exibicao from './Exibicao'
 
 function App() {
 
   return (
     <div className='app'>
+
       <Exibicao abertura={new Date("2024-11-06T08:00.000-3:00")}
       fechamento={new Date("2024-11-06T16:00.000-3:00")}
-      cercado="A1" />
-      <Animal icone="" nome="Leão" peso={300} extincao/>
-      <Animal icone="" nome="Girafa" peso={1000} extincao/>
+      cercado="A1">
+        <Animal icone="🦁" nome="Leão" peso={300} extincao/>
+        <Animal icone="🦒" nome="Girafa" peso={1000} extincao/>
+      </Exibicao>
+      <Exibicao abertura={new Date("2024-11-06T13:00.000-3:00")}
+      fechamento={new Date("2024-11-06T17:00.000-3:00")}
+      cercado="B1">
+      <Animal icone="🦜" nome="Papagaio" peso={0.12} extincao/>
+      </Exibicao>
     </div>
     
   )
--- src/Exibicao.tsx
+++ src/Exibicao.tsx
@@ -1,16 +1,20 @@
+import { ReactNode } from 'react';
 import './Exibicao.css';
 interface ExibicaoProps {
     abertura: Date;
     fechamento: Date;
     cercado: string;
+    children: ReactNode
 }
 
-export default function ({abertura:inicio, fechamento:fim, cercado:local}: ExibicaoProps){
+export default function ({abertura:inicio, fechamento:fim, cercado:local, children:animais
+}: ExibicaoProps){
     return (
     <div className="exibicao">
         <div className="inicio">Abertura: {inicio.toLocaleDateString()}</div>
         <div className="fim">Fechamento: {fim.toLocaleDateString()}</div>
         <div className="local">Cercado: {local}</div>
+        <div className='animais'>{animais}</div>
     </div>
     );
 }
